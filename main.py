92346546f53e796ca784f8ae8b3eeede0e0ca284
caro8464 <carolina.wehrmann@gmx.de>
2020-07-15 18:19:07 +0200

    readd elbow plot

--- main.py
+++ main.py
@@ -23,22 +23,17 @@ for k in K:
     km = km.fit(data_scaled)
     added_squared_distances.append(km.inertia_)
 
-# plt.plot(K, added_squared_distances, 'bx-')
-# plt.xlabel('k')
-# plt.ylabel('Sum_of_squared_distances')
-# plt.title('Elbow Method For Optimal k')
-# plt.show()
+plt.plot(K, added_squared_distances, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Sum_of_squared_distances')
+plt.title('Elbow Method For Optimal k')
+plt.show()
 
 # based on the plot the optimal K seems to be 5 taking the elbow-effect into account
 # A) the best K is 5
 
 
 X, y = make_blobs(n_samples = 100, n_features=3, centers=5)
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y)
-
-plt.show()
 
 km = KMeans(n_clusters=5)
 km = km.fit(X)
@@ -47,9 +42,9 @@ labels = km.predict(X)
 C = km.cluster_centers_
 
 # prints our clustering vectors to the console
-for idx,vector in enumerate(C, start=1):
-    print("\nVector number {}: X={}, Y={}, Z={}" .format(idx, vector[0], vector[1], vector[2]))
-
+for idx,vector in enumerate(C, start=0):
+    print("\nCluster index {}: X={}, Y={}, Z={}" .format(idx, vector[0], vector[1], vector[2]))
+print("\nCluster index for each row:\n",labels)
 
 # now show our centers (clustering vectors) in the 3D graph
 fig = plt.figure()
